fa7e269de270d9057d13ae3a844c7177e915209c
Sniffleupagus <midden_sofas0k@icloud.com>
2024-02-23 23:55:07 +0000

    pwny-hydra, first commit. make filename, hostname, and api address configurable in config.toml

--- pwnagotchi/ui/web/__init__.py
+++ pwnagotchi/ui/web/__init__.py
@@ -1,7 +1,13 @@
 import os
 from threading import Lock
 
-frame_path = '/var/tmp/pwnagotchi/pwnagotchi.png'
+# pwny-hydra - move frame_path to /tmp (truly temporary, not retained in /var/tmp), and put PID
+# in filename to support multiple pwnagotchi on one system. Additional images will accumulate in
+# /tmp until a reboot. in /var/tmp, they would accumulate until deleted. Potentially "breaking"
+# change, since the semi-permanent "/var/tmp/pwnagotchi.png" is no longer available. I am unaware
+# of any plugins or other code that accesses the png directly, so I think it is ok.
+
+frame_path = '/tmp/pwnagotchi/pwnagotchi%d.png' % os.getpid()
 frame_format = 'PNG'
 frame_ctype = 'image/png'
 frame_lock = Lock()
